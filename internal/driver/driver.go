b431522f0bd1ece6134863e2c90510ca2bd9567d
vrachev <vlad.rachev@mongodb.com>
2017-07-14 17:54:09 -0400

    removing change I made to check git push

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -132,7 +132,7 @@ func generateReport(p *profile.Profile, cmd []string, vars variables, o *plugin.
 		return err
 	}
 
-	// Output to specified file. s
+	// Output to specified file.
 	o.UI.PrintErr("Generating report in ", output)
 	out, err := os.Create(output)
 	if err != nil {
